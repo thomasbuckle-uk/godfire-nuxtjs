fa7f2eff337d36e5b7acc2e742ce31a4f5e68f8b
Thomas Buckle <thomasbuckle.uk@outlook.com>
2021-10-24 05:07:06 +0100

    Applied background image

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,19 @@
 <template>
-  <div class="home"></div>
+  <div class="home bg-cover	" :style="{'background-image': `url(${require('@/assets/images/rival-bg-default.jpg')})`}">
+    <div class="" ></div>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+
+})
 </script>
+<style>
+
+.home {
+  min-height: 100vh;
+}
+</style>
